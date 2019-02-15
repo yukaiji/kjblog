60c8594dc6965fac75a512958a275fe9dae8fbe9
kaijiyu@creditease.cn <kaijiyu@creditease.cn>
2019-02-15 16:06:39 +0800

    增加MVC和静态资源的配置

---
+++ src/main/java/com/yukaiji/kjblog/config/MvcConfig.java
@@ -0,0 +1,26 @@
+package com.yukaiji.kjblog.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+@Configuration
+public class MvcConfig extends WebMvcConfigurationSupport {
+    /**
+     * 默认首页的跳转
+     */
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("forward:index");
+    }
+
+    /**
+     * 设置静态资源，防止被拦截
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+        registry.addResourceHandler("/editor/**").addResourceLocations("classpath:/static/editor/");
+    }
+}
--- src/main/java/com/yukaiji/kjblog/controller/WriteArticleController.java
+++ src/main/java/com/yukaiji/kjblog/controller/WriteArticleController.java
@@ -1,12 +1,19 @@
 package com.yukaiji.kjblog.controller;
 
+import com.alibaba.fastjson.JSONObject;
 import com.yukaiji.kjblog.model.responsemodel.BaseResponse;
+import org.apache.tomcat.util.bcel.classfile.Constant;
 import org.springframework.http.HttpRequest;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author kaijiyu
@@ -24,4 +31,33 @@ public class WriteArticleController extends BaseController{
         printJson(response, baseResponse);
     }
 
+    @RequestMapping("/uploadfile")
+    public void uploadfile(@RequestParam(value = "editormd-image-file", required = true) MultipartFile file,
+                           HttpServletRequest request, HttpServletResponse response){
+        BaseResponse baseResponse = new BaseResponse();
+        String trueFileName = file.getOriginalFilename();
+
+        String suffix = trueFileName.substring(trueFileName.lastIndexOf("."));
+
+        String fileName = System.currentTimeMillis()+suffix;
+
+        String path = request.getSession().getServletContext().getRealPath("/assets/msg/upload/");
+        System.out.println(path);
+
+        File targetFile = new File(path, fileName);
+        if(!targetFile.exists()){
+            targetFile.mkdirs();
+        }
+        //保存
+        try {
+            file.transferTo(targetFile);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        JSONObject res = new JSONObject();
+        res.put("url","");
+        res.put("success", 1);
+        res.put("message", "upload success!");
+        printJson(response, baseResponse);
+    }
 }
